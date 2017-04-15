228d240adc28db9e4360879c868b0871d487714d
AHijazi <AHijazi@users.noreply.github.com>
2017-04-15 15:17:59 +0400

    fully functional

--- auduino_file.ino
+++ auduino_file.ino
@@ -1,5 +1,8 @@
+zakaria.belk [11:17 AM] 
 volatile int flow_frequency; // Measures flow sensor pulses
-unsigned int l_hour; // Calculated litres/hour
+ float l_sec; // Calculated litres/hour
+ float lastAmount=0;;
+ float newAmount=0;
 unsigned char flowsensor = 2; // Sensor Input
 unsigned long currentTime;
 unsigned long cloopTime;
@@ -10,7 +13,14 @@ void flow () // Interrupt function
 void setup()
 {
    pinMode(flowsensor, INPUT);
-   digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
+   pinMode(3,OUTPUT);
+   pinMode(4,OUTPUT);
+   pinMode(6,OUTPUT);
+   pinMode(8,OUTPUT);
+   pinMode(10,OUTPUT);
+   pinMode(12,OUTPUT);
+
+  digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
    Serial.begin(9600);
    attachInterrupt(0, flow, RISING); // Setup Interrupt
    sei(); // Enable interrupts
@@ -19,15 +29,92 @@ void setup()
 }
 void loop ()
 {
-   currentTime = millis();
-   // Every second, calculate and print litres/hour
+
+
+  currentTime = millis();
+   // Every 2 seconds, calculate and print litres/hour
    if(currentTime >= (cloopTime + 1000))
    {
+    
       cloopTime = currentTime; // Updates cloopTime
       // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
-      l_hour = (flow_frequency * 60 / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
+      l_sec = (flow_frequency  / (60*7.5)); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
       flow_frequency = 0; // Reset Counter
-      Serial.print(l_hour, DEC); // Print litres/hour
-      Serial.println(" L/hour");
+      newAmount=lastAmount+(l_sec);
+      Serial.print(l_sec); // Print litres/hour
+      Serial.print(" L/sec");
+      Serial.print("---");
+      Serial.print(newAmount);
+      Serial.println(" L");
+
+  }    
+  if(l_sec==0){
+    delay(6000);
+    if(l_sec==0){
+    newAmount=0;
+    lastAmount=0;
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(12,HIGH);      
    }
+    }
+  
+
+   if(newAmount > 0.2 && newAmount < 0.4){
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(12,LOW);
+    }
+
+   if(newAmount > 0.4 && newAmount < 0.6){
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(12,LOW);
+    }
+    if(newAmount > 0.6 && newAmount < 0.8){
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(10,LOW);
+    digitalWrite(12,LOW);
+    }
+    
+   if(newAmount > 0.8 && newAmount < 1){
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(8,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(12,LOW);
+    }
+    if(newAmount > 1 && newAmount < 1.2){
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(12,LOW);
+    }
+
+   if(newAmount > 1.2){
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(6,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(12,LOW);
+    }
+
+   lastAmount=newAmount;
+   
 }
